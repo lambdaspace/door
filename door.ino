138bc5c5822231e76faa6bc8d7843f8ebb0aa672
Christos Bacharakis <christos@bacharakis.com>
2015-11-26 02:39:19 +0200

    Added time delay for unknown cards

--- door.ino
+++ door.ino
@@ -18,6 +18,8 @@ void setup()
 //claire's card
 long keys[] = { 1234567, 1234568, 1234569 };
 
+int unknown_card = 0;
+bool success = false;
 
 void loop()
 {
@@ -35,14 +37,53 @@ void loop()
 			if(keys[i] == card)
 			{
 				Serial.print("Welcome: ");
-        Serial.println(card);
+				Serial.println(card);
 				digitalWrite(5, HIGH);
 				//wait 2 seconds before lock the door
 				delay(2000);
+				success=true;
+				unknown_card=0;
 				break;
 			}
-      Serial.print("Unknown: ");
-      Serial.println(card);
+		}
+		if(success == false)
+		{
+			unknown_card++;
+			Serial.print("Unknown:");
+			Serial.println(card);
+			switch (unknown_card)
+			{
+				case 1:
+					delay(1000);
+					break;
+				case 2:
+					delay(1000);
+					break;
+				case 3:
+					Serial.print("WARNING! Intrusion detected: 3\n");
+					delay(1000);
+					break;
+				case 4:
+					Serial.print("WARNING! Intrusion detected: 4\n");
+					delay(2000);
+					break;
+				case 5:
+					Serial.print("WARNING! Intrusion detected: 5\n");
+					delay(2000);
+					break;
+				case 6:
+					Serial.print("WARNING! Intrusion detected: 6\n");
+					delay(3000);
+					break;
+				case 7:
+					Serial.print("WARNING! Intrusion detected: 7\n");
+					break;
+					delay(5000);
+				default:
+					Serial.print("WARNING! Intrusion detected: default\n");
+					delay(10000);
+					break;
+			}
 		}
 	}
 }
